0e765cb9f04dbd973196f0e5d5d7e7e267525910
Vijay <vijay@iavian.com>
2025-06-28 14:51:27 +0530

    Function refactor

--- wasm.sh
+++ wasm.sh
@@ -3,7 +3,7 @@
 emcmake cmake -G Ninja . -B build_wasm -DEXIV2_ENABLE_INIH=OFF -DEXIV2_ENABLE_XMP=OFF -DBROTLI_INCLUDE_DIR=/Users/iavian/Downloads/brotli/c/include \
                                        -DBROTLICOMMON_LIBRARY=/Users/iavian/Downloads/brotli/build_wasm/libbrotlicommon.a \
                                        -DBROTLIDEC_LIBRARY=/Users/iavian/Downloads/brotli/build_wasm/libbrotlidec.a \
-                                       -DIS_WASM:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_CXX_FLAGS="-std=c++14" -DCMAKE_EXE_LINKER_FLAGS="-g2 -static -sERROR_ON_UNDEFINED_SYMBOLS=1 -Os -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s FORCE_FILESYSTEM=1 -s ENVIRONMENT=web -s"
+                                       -DIS_WASM:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_CXX_FLAGS="-std=c++14" -DCMAKE_EXE_LINKER_FLAGS="-g0 -static -sERROR_ON_UNDEFINED_SYMBOLS=1 -Os -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s FORCE_FILESYSTEM=1 -s ENVIRONMENT=web"
 
 cd build_wasm || exit
 
